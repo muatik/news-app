94fe3f2c512ef4ba8b4874ed4969ffffd135e720
muatik <muatik@gmail.com>
2017-04-29 23:50:21 +0300

    adds publishDate and keywords fields to article entity

--- src/main/java/com/muatik/model/Article.java
+++ src/main/java/com/muatik/model/Article.java
@@ -1,10 +1,13 @@
 package com.muatik.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -13,16 +16,31 @@ import java.util.Set;
 @Data
 @Entity
 public class Article {
-    @Id
-    @GeneratedValue
-    private long id;
+    @NotNull
+    @Column(length = 255)
+    private String header;
 
     @NotNull
     private String text;
+
+    /**
+     * this filed is indexed because there is need to list all articles
+     * for a given period
+     */
+    @NotNull
+    @Column(name = "publish_date")
+    @JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+    private Date publishDate;
 
     // an article may have more than one author.
     // authors should already be persisted.
     @NotEmpty
     @ManyToMany(cascade = CascadeType.MERGE)
     Set<Author> authors;
+
+    // I assume that keywords at least should not be longer than 255.
+    // probably it will be shorter but not specified in the project description.
+    @ElementCollection
+    @Column(length = 255)
+    Set<String> keywords = new HashSet<>();
 }
